4e5ad09dbe2d41288d315adcb3fad40f37858db6
scole <coles2@hartwick.edu>
2020-04-03 08:49:33 -0400

    fixed it

--- PalindromeCheck.py
+++ PalindromeCheck.py
@@ -3,19 +3,22 @@
 
 
 def is_palindrome(word):
-    word.strip()
-    word.lower()
-    if len(word) == 1 or 0:
+    thisword = word.strip()
+    thisword = word.lower()
+    if len(thisword) == 1 or 0:
         return True
     else:
-        if word[0] == word[-1]:
-            return is_palindrome(word[1:-1])
+        if thisword[0] == thisword[-1]:
+            return is_palindrome(thisword[1:-1])
         else:
             return False
 
 
-word = str(input("Enter a word you think might be a palindrome"))
-if is_palindrome(word):
-    print("This word is a Palindrome")
-else:
-    print("This word isnt a Palindrome")
+answer = 1
+while int(answer) != 0:
+    word = str(input("Enter a word you think might be a palindrome"))
+    if is_palindrome(word):
+        print("This word is a Palindrome")
+    else:
+        print("This word isnt a Palindrome")
+    answer = input("Do you want to continue? 1 for yes 0 for no")
